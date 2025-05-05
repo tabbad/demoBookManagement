f8efc401a66445ec1f23e5fe447a3946d38c885f
Jean-Clément Sabourin <j.sabourin@betclicgroup.com>
2025-05-05 15:21:48 +0200

    Integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,24 @@ repositories {
     mavenCentral()
 }
 
+testing {
+    suites {
+        val testIntegration by registering(JvmTestSuite::class) {
+            sources {
+                kotlin {
+                    setSrcDirs(listOf("src/testIntegration/kotlin"))
+                }
+                compileClasspath += sourceSets.main.get().output
+                runtimeClasspath += sourceSets.main.get().output
+            }
+        }
+    }
+}
+
+val testIntegrationImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.implementation.get())
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -46,6 +64,21 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     testImplementation("io.kotest.extensions:kotest-extensions-pitest:1.2.0")
+
+    testIntegrationImplementation("io.mockk:mockk:1.13.8")
+    testIntegrationImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testIntegrationImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testIntegrationImplementation("com.ninja-squad:springmockk:4.0.2")
+    testIntegrationImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
+    testIntegrationImplementation("org.testcontainers:postgresql:1.19.1")
+    testIntegrationImplementation("org.testcontainers:jdbc-test:1.12.0")
+    testIntegrationImplementation("org.testcontainers:testcontainers:1.19.1")
+    testIntegrationImplementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
+    testIntegrationImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+    testIntegrationImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+    testIntegrationImplementation("io.kotest.extensions:kotest-extensions-pitest:1.2.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
 }
